8221f9a83d65380af3299522cc34e7ff073a449a
Jonas Hessling <firstname.jonas@icloud.com>
2024-04-14 07:41:30 +0000

    Deploy app to heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-)m0do=0t2xe-^#c*(-t)&-9$hv&%7v+^&0+=xz=8-_38e&p%q5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-firstnamejo-boutiqueado-xsale1ayrme.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['jonas-boutique-ado.herokuapp.com', '8000-firstnamejo-boutiqueado-xsale1ayrme.ws-eu110.gitpod.io']
 
 
 # Application definition
@@ -115,15 +116,19 @@ WSGI_APPLICATION = 'boutique_ado.wsgi.application'
 
 
 # Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
